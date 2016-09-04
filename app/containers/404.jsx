936c74b4dfdd1f3a551bb0fb2b2cec3fcac77a2b
wangfupeng1988 <wangfupeng1988@163.com>
2016-09-04 16:33:14 +0800

    update

--- app/containers/404.jsx
+++ app/containers/404.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 class NotFound extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
     render() {
         return (
             <h1>404</h1>
--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 class Home extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
     render() {
         return (
             <div>
