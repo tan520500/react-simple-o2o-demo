98dcced28de7d7619897164591a4a5ab7e4cc7b0
wangfupeng1988 <wangfupeng1988@163.com>
2016-10-23 16:37:40 +0800

    update

--- app/containers/Detail/index.jsx
+++ app/containers/Detail/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
+import Header from '../../components/Header'
+import Info from './subpage/Info'
+import Comment from './subpage/Comment'
+
 class Detail extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
+        // 获取商户ID
+        const id = this.props.params.id
+
         return (
             <div>
-                <h1>Detail</h1>
+                <Header title="商户详情" type="share"/>
+                <Info id={id}/>
+                <Comment id={id}/>
             </div>
         )
     }
 }
-
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
-// export default Detail
-module.exports = Detail
+ 
+export default Detail
